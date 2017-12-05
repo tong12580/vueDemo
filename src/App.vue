40bd2998013b3d624f25acef8b998654df15a40e
yutong <467942250@qq.com>
2017-12-05 21:14:52 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="app">
+        <img src="./assets/logo.png">
+        <router-view/>
+    </div>
+</template>
+
+<style src="./style.scss" lang="scss"></style>
+<script>
+  export default {
+    name: 'app'
+  }
+</script>
